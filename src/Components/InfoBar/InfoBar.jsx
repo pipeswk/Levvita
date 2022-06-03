6877d9b8150123fcf449d6ce03201965d7c6502a
pipeswk <afco1995@gmail.com>
2022-06-02 19:58:11 -0500

    Componente infoBar v1

---
+++ src/Components/InfoBar/InfoBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './InfoBar.module.css'
+
+const InfoBar = () => {
+  return (
+    <div className={styles.infoBar}>
+        ENVIO GRATIS A PARTIR DE $50.000 - PAGA CONTRA ENTREGA
+    </div>
+  )
+}
+
+export default InfoBar
