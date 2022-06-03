bc7c9a19bc26423a02f50ec0da4bb075c4f0457b
pipeswk <afco1995@gmail.com>
2022-06-02 19:57:16 -0500

    Componente CategoriaInicio v1

---
+++ src/Components/CategoriaInicio/CategoriaInicio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './CategoriaInicio.module.css'
+
+const CategoriaInicio = () => {
+  return (
+    <div className='mx-3'>
+        <img className={styles.circularImg} src="/img/Zapatillas-de-correr-para-hombre-y-Mujer-zapatos-vulcanizados-de-malla-transpirable-ligeros-color-blanco-gran.jpg_640x640_1_480x480.webp" alt="" />
+    </div>
+  )
+}
+
+export default CategoriaInicio
