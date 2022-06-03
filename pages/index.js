86cbc2449168861258efd4477733a6fed17ce512
pipeswk <afco1995@gmail.com>
2022-06-03 18:44:34 -0500

    Se agrega Hero y Reticula al Home

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Layout from '../src/Layouts/Layout.jsx'
 import Slider from '../src/Components/Slider/Slider'
 import CategoriasInicio from '../src/Components/CategoriasInicio/CategoriasInicio.jsx'
+import Hero from '../src/Components/Hero/Hero.jsx'
+import Reticula from '../src/Components/Reticula/Reticula.jsx'
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
     >
       <Slider />
       <CategoriasInicio />
+      <Hero />
+      <Reticula />
     </Layout>
   )
 }
