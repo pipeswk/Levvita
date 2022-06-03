ed5ff38cb646c121eab6f221e63b34d16867c488
pipeswk <afco1995@gmail.com>
2022-06-02 19:57:57 -0500

    Componente HorizontalWrapper v1

---
+++ src/Components/HorizontalWrapper/HorizontalWrapper.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './HorizontalWrapper.module.css'
+
+const HorizontalWrapper = ( { children } ) => {
+  return (
+    <div className='container-fluid mt-3'>
+        <div className='row'>
+            <div className={`col-12 w-100 d-flex justify-content-center ${styles.horizontalScrolling_Wrapper}`}>
+                {children}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HorizontalWrapper
