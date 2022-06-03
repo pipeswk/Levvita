c8041294ef6d96638a6705fe9861b12d51bd942f
pipeswk <afco1995@gmail.com>
2022-06-02 19:58:59 -0500

    Componente MenuHamburguesa v1

---
+++ src/Components/MenuHamburguesa/MenuHamburguer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MenuHamburguer = () => {
+  return (
+    <div className='d-lg-none'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+        </svg>
+    </div>
+  )
+}
+
+export default MenuHamburguer
