573af9c7d1cac1bfb082295891fa377018c72bc2
pipeswk <afco1995@gmail.com>
2022-06-02 19:59:22 -0500

    Componente MenuTexto v1

---
+++ src/Components/MenuTexto/MenuTexto.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const MenuTexto = () => {
+  return (
+    <div className='d-none d-lg-block'>
+        <ul className='navbar-nav me-auto mb-2 mb-md-0'>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Opcion 1</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Opcion 2</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Opcion 3</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Opcion 4</a>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default MenuTexto
