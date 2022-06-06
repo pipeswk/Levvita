f865a64a1e49f6da3c3b92ff1f72f142f130da2f
pipeswk <afco1995@gmail.com>
2022-06-06 14:50:46 -0500

    Se leen datos via props y se itera

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,8 +1,18 @@
+import { useState, useEffect } from 'react'
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
-import styles from './Slider.module.css'
+import Slide from '../Slide/Slide';
 
-const Slider = () => {
+const Slider = ( { slider } ) => {
+
+    const [slides, setSlides] = useState([]);
+
+
+    useEffect(() => {
+        setSlides(slider)
+    }, [])
+    
+
   return (
     <Splide
         options={{
@@ -13,26 +23,12 @@ const Slider = () => {
             gap: '1rem'
         }}
     >
-        <SplideSlide>
-            {/* Oculto en pequeñas pantallas */}
-            <div className={`${styles.imageDesktop} d-none d-lg-block`}>
-                <img className={`img-fluid ${styles.img}`} src="/img/¿puedo-hacer-pedidos-por-anticipado-de-productos-nike-ayuda-de-nike.jpg" alt="Image 1"/>
-            </div>
-            {/* Oculto en grandes pantallas */}
-            <div className='d-lg-none' >
-                <img src="/img/Zapatillas-de-correr-para-hombre-y-Mujer-zapatos-vulcanizados-de-malla-transpirable-ligeros-color-blanco-gran.jpg_640x640_1_480x480.webp" alt="Image 1"/>
-            </div>
-        </SplideSlide>
-        <SplideSlide>
-            <div className='d-lg-none' >
-                <img src="/img/2.webp" alt="Image 1"/>
-            </div>
-        </SplideSlide>
-        <SplideSlide>
-            <div className='d-lg-none' >
-                <img src="/img/3.webp" alt="Image 1"/>
-            </div>
-        </SplideSlide>
+        {slides.map((slide, index) => (
+            <Slide
+                key={index}
+                slide={slide}
+            />
+        ))}
     </Splide>
   )
 }
