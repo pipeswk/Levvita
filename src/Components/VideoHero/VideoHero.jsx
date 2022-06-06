aece8589b4d88f26fdbe5ac8be7238625bd348c3
pipeswk <afco1995@gmail.com>
2022-06-06 14:51:26 -0500

    Se leen datos via props

--- src/Components/VideoHero/VideoHero.jsx
+++ src/Components/VideoHero/VideoHero.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import ReactPlayer from 'react-player/lazy'
 
-const VideoHero = () => {
+const VideoHero = ( { video_src } ) => {
   return (
     <div>
         {/* //TODO: SOLUCIONAR PROBLEMA DE AUTOPLAY */}
-        {/* <ReactPlayer url={[
-            {src: '/img/Video.mp4', type: 'video/mp4'}
-        ]} loop playing={true} width='100%' height='100%' volume={0} /> */}
         <iframe
             width="100%"
             height="400"
-            src="https://www.youtube.com/embed/NljGurzPM2g?autoplay=1"
+            src={video_src}
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
