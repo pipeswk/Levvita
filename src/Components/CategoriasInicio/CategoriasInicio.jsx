25d355427ceee9575ddc24091739b6c267bca693
pipeswk <afco1995@gmail.com>
2022-07-06 00:25:03 -0500

    Nuevo prop

--- src/Components/CategoriasInicio/CategoriasInicio.jsx
+++ src/Components/CategoriasInicio/CategoriasInicio.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import CategoriaInicio from '../CategoriaInicio/CategoriaInicio'
 import HorizontalWrapper from '../HorizontalWrapper/HorizontalWrapper'
 
-const CategoriasInicio = ( { categorias } ) => {
+const CategoriasInicio = ( { type, title, categorias } ) => {
 
   return (
     <div className='mt-4'>
-        <h2 className='text-center'>¿Para quien compras?</h2>
-        {/* //TODO: Mapear con la base de datos */}
+        <h2 className='text-center'>{title}</h2>
         <HorizontalWrapper>
           {categorias.map( (categoria, index) => (
             <CategoriaInicio
               key={index}
+              type={type}
               categoria={categoria}
             />
           ))}
