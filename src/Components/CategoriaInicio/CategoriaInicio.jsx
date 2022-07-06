08fe0713dd9f2280dd4c0a11f52e6fdd90b51925
pipeswk <afco1995@gmail.com>
2022-07-06 00:24:51 -0500

    Funcion clic

--- src/Components/CategoriaInicio/CategoriaInicio.jsx
+++ src/Components/CategoriaInicio/CategoriaInicio.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import styles from './CategoriaInicio.module.css'
+import { useRouter } from 'next/router'
 
-const CategoriaInicio = ( { categoria } ) => {
+const CategoriaInicio = ( { type, categoria } ) => {
 
   const { title, img_src, to_url } = categoria
+  const router = useRouter()
+
+  const handleClick = () => {
+    if (type === 'subcategory') {
+      console.log('Subcategoría')
+    } else {
+      router.push(to_url);
+    }
+  }
 
   return (
-    <div className='mx-3 d-flex-none'>
+    <div className='mx-3 d-flex-none' role='button' onClick={handleClick}>
       <img className={styles.circularImg} src={img_src} alt="Imagen Categoria" />
       <p className='text-center'>{title}</p>
     </div>
