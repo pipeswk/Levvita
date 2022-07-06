1d65dc1969e5a15aa8dac4e0e1a11c66e67727fc
pipeswk <afco1995@gmail.com>
2022-07-06 00:25:53 -0500

    Se alista componente para iterar con info del server

--- src/Components/Reticula/Reticula.jsx
+++ src/Components/Reticula/Reticula.jsx
@@ -1,48 +1,13 @@
 import React from 'react'
 import Card from '../Card/Card';
 
-const Reticula = () => { 
+const Reticula = ( { data, title } ) => { 
 
   return (
     <div className='container'>
-        <h1 className='fw-bold d-flex justify-content-center mb-5 fs-1'>Novedades</h1>
+        <h1 className='fw-bold d-flex justify-content-center mb-5 fs-1'>{title}</h1>
         <div className='row'>
             {/* //TODO: ITERAR CON LA BASE DE DATOS */}
-            <Card
-                movil={2}
-                tablet={3}
-                compu={4}
-            />
-            <Card
-                movil={2}
-                tablet={3}
-                compu={4}
-            />
-            <Card
-                movil={2}
-                tablet={3}
-                compu={4}
-            />
-            <Card
-                movil={2}
-                tablet={3}
-                compu={4}
-            />
-            <Card
-                movil={2}
-                tablet={3}
-                compu={4}
-            />
-            <Card
-                movil={2}
-                tablet={3}
-                compu={4}
-            />
-            <Card
-                movil={2}
-                tablet={3}
-                compu={4}
-            />
             <Card
                 movil={2}
                 tablet={3}
