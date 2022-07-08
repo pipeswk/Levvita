a97246fc95523214d711c4e27ff2d0b28f629c27
pipeswk <afco1995@gmail.com>
2022-07-07 19:54:31 -0500

    Se alimenta Card con info del server

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -6,16 +6,17 @@ const Card = ( { data, movil, tablet, compu } ) => {
     const mov = 12/movil;
     const tab = 12/tablet;
     const pc = 12/compu;
+    const { title, price, main_img, comparative_price } = data;
 
   return (
     <div className={`col-${mov} col-md-${tab} col-lg-${pc}`}>
-       <div className='card mb-4 position-relative' role='button' >
-            <img src='/img/Zapatillas-de-correr-para-hombre-y-Mujer-zapatos-vulcanizados-de-malla-transpirable-ligeros-color-blanco-gran.jpg_640x640_1_480x480.webp' className={`card-img-top img-fluid ${styles.img}`} alt='...' />
+       <div className={`card mb-4 position-relative ${styles.card}`} role='button' >
+            <img src={main_img} className={`card-img-top img-fluid ${styles.img}`} alt='...' />
             <div className='card-body'>
-                <h5 className='card-title fs-5'>Twizz® 2022 - Sneakers Ultra Confortables</h5>
+                <h5 className={`card-title ${styles.title}`}>{title}</h5>
                 <div className='d-flex gap-2'>
-                    <p className='card-text fw-bold text-primary'>$119.900</p>
-                    <p className='card-text text-decoration-line-through fw-bold text-danger'>$219.800</p>
+                    <p className='card-text fw-bold text-primary'>{price}</p>
+                    <p className='card-text text-decoration-line-through fw-bold text-danger'>{comparative_price}</p>
                 </div>
             </div>
             <span className={`position-absolute top-0 start-50 translate-middle badge p-2 ${styles.badge}`}>Oferta</span>  
