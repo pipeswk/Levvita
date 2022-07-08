95c1646007d4a72cb719f71237a7dc799f7856ab
pipeswk <afco1995@gmail.com>
2022-07-07 19:55:05 -0500

    Nuevas funciones parafiltrar en categoria

--- src/Components/CategoriaInicio/CategoriaInicio.jsx
+++ src/Components/CategoriaInicio/CategoriaInicio.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import { useState } from 'react'
 import styles from './CategoriaInicio.module.css'
 import { useRouter } from 'next/router'
+import useLevvita from '../../Hooks/useLevvita'
 
-const CategoriaInicio = ( { type, categoria } ) => {
-
+const CategoriaInicio = ( { type, categoria, setCatSeleccionada, catSeleccionada } ) => {
   const { title, img_src, to_url } = categoria
   const router = useRouter()
+  const { 
+    setOffListenerCategory,
+    setOffListenerSubcategory, 
+    getProductsCategory, 
+    getProductsSubcategory 
+  } = useLevvita();
+  
 
   const handleClick = () => {
     if (type === 'subcategory') {
-      console.log('Subcategoría')
+      setCatSeleccionada(title);
+      if (title !== 'Todo') {
+        setOffListenerCategory();
+        getProductsSubcategory(title);
+      } else {
+        setOffListenerSubcategory();
+        getProductsCategory();
+      }
     } else {
       router.push(to_url);
     }
@@ -17,8 +31,8 @@ const CategoriaInicio = ( { type, categoria } ) => {
 
   return (
     <div className='mx-3 d-flex-none' role='button' onClick={handleClick}>
-      <img className={styles.circularImg} src={img_src} alt="Imagen Categoria" />
-      <p className='text-center'>{title}</p>
+      <img className={type === 'subcategory' && catSeleccionada === title?styles.circularImg_active:styles.circularImg} src={img_src} alt="Imagen Categoria" />
+      <p className={`${type === 'subcategory' && catSeleccionada === title?styles.title_active:styles.title} text-center`}>{title}</p>
     </div>
   )
 }
