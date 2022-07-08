58cbae44004af851bcb302cbef66dd144ca7a667
pipeswk <afco1995@gmail.com>
2022-07-07 19:56:05 -0500

    Se mapean productos con info del servidor

--- src/Components/Reticula/Reticula.jsx
+++ src/Components/Reticula/Reticula.jsx
@@ -3,16 +3,22 @@ import Card from '../Card/Card';
 
 const Reticula = ( { data, title } ) => { 
 
+  console.log(data);
+
   return (
     <div className='container'>
         <h1 className='fw-bold d-flex justify-content-center mb-5 fs-1'>{title}</h1>
         <div className='row'>
             {/* //TODO: ITERAR CON LA BASE DE DATOS */}
-            <Card
+            {data.map(product => (
+              <Card
+                key={product.id}
+                data={product}
                 movil={2}
                 tablet={3}
                 compu={4}
             />
+            ))}
         </div>
     </div>
   )
