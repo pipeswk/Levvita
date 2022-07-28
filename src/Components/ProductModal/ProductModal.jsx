77f1f801e3773527097c2e5b4d7e34f6e733681a
pipeswk <afco1995@gmail.com>
2022-07-27 21:23:06 -0500

    Componente ProductModal v1

---
+++ src/Components/ProductModal/ProductModal.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import useLevvita from '../../Hooks/useLevvita'
+import ProductOptions from '../ProductOptions/ProductOptions'
+
+const ProductModal = ( { options } ) => {
+
+    const {
+        setSelectColor,
+        colorSelected,
+        setColor,
+        selectSize,
+        setSelectSize
+    } = useLevvita();
+
+  return (
+    <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-fullscreen-sm-down">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">Título del modal</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                    <ProductOptions
+                        colores={options.colores}
+                        sizes={options.tallas}
+                        setSelectColor={setSelectColor}
+                        colorSelected={colorSelected}
+                        setColor={setColor}
+                        selectSize={selectSize}
+                        setSelectSize={setSelectSize}
+                    />
+                </div>
+                <div className="modal-footer">
+                    {/* <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="button" className="btn btn-primary">Guardar cambios</button> */}
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductModal
